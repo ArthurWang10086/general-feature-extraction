a02252f1525c80d8dc617560b8e0a7482050feca
kuanke <asdqsczser@gmail.com>
2018-05-10 13:49:18 +0800

    debug

--- Utils/GetAllFiles.py
+++ Utils/GetAllFiles.py
@@ -1,9 +1,9 @@
 import os
-class GetAllFiles(object):
-    def getAllFiles(self,dir):
-        L=[]
-        for root, dirs, files in os.walk(dir):
-            for file in files:
-                if os.path.splitext(file)[1] == '.json':
-                    L.append(os.path.join(root, file))
-        return L
+def GetAllFiles(self,dir):
+    L=[]
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if os.path.splitext(file)[1] == '.json':
+                L.append(os.path.join(root, file))
+    return L
+
