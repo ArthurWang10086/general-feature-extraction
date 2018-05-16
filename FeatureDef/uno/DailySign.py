3e8fb2cf92f8718e707380075a003c2da547db76
kuanke <asdqsczser@gmail.com>
2018-05-16 15:15:37 +0800

    debug

--- FeatureDef/uno/DailySign.py
+++ FeatureDef/uno/DailySign.py
@@ -1,6 +1,7 @@
 from Feature.FeatureCount import FeatureCount
 from Feature.Feature import Feature
-
+import datetime
+import time
 # DailySign_freq - 签到频度 DailySign
 class DailySign_freq(FeatureCount):
     def __init__(self):
@@ -10,5 +11,10 @@ class DailySign_time(Feature):
     def __init__(self):
         Feature.__init__(self, 'DailySign_time', 'DailySign')
 
+    def datetime_timestamp(self,dt):
+        time.strptime(dt,'%Y-%m-%d %H:%M:%S')
+        s = time.mktime(time.strptime(dt,'%Y-%m-%d %H:%M:%S'))
+        return s
+
     def __parse__(self, item):
-        return item['timestamp']
+        return int(self.datetime_timestamp(item['timestamp']))
