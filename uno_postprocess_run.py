55ea53c5a9121ae5b44c62912827f203d26f270f
kuanke <asdqsczser@gmail.com>
2018-05-18 13:58:40 +0800

    debug

--- uno_postprocess_run.py
+++ uno_postprocess_run.py
@@ -84,7 +84,9 @@ if __name__=='__main__':
 
     hive_featurenames = ['SerialId','getusetimemin','gettimeaverage'
         ,'getusetimemax' ,'postusetimemin','posttimeaverage'
-        ,'postusetimemax','unocount','date']
+        ,'postusetimemax','unocount','getcount','postcount','postovercount'
+        ,'postovercountmagic','postovercountpower','arguecount'
+        ,'arguehappencount','arguesuccesscount','roomplaylogcount','date']
     df_hive=pd.read_table(hive_feature_filename
                           ,sep='\t'
                           ,names=hive_featurenames
@@ -93,16 +95,36 @@ if __name__=='__main__':
 
     df = df_log.merge(df_hive,how = 'inner')
     df = df[df['UnoRoomPlayReviewOneLog_timeconsume_average']>0]
+    df['roomplaylogcount'][df['roomplaylogcount']<0] = df['roomplaylogcount'][df['roomplaylogcount']<0]+256
     df['UnoRoomPlayReviewOneLog_postusetime_four_ratio']= df['UnoRoomPlayReviewOneLog_timeconsume_average']/df['posttimeaverage']
     df['UnoRoomPlayReviewOneLog_postusetime_four_ratio'][df['posttimeaverage']<1]=-1
     # df_log['UnoRoomPlayReviewOneLog_post_ratio']= df_log['UnoRoomPlayReviewOneLog_post']/df_log['posttimeaverage']
     # df_log['UnoRoomPlayReviewOneLog_post_ratio'][df_log['posttimeaverage']<1]=-1
     df['UnoRoomPlayReviewOneLog_unomay_four_ratio']= df['UnoRoomPlayReviewOneLog_unomay']/df['unocount']
     df['UnoRoomPlayReviewOneLog_unomay_four_ratio'][df['unocount']<1]=-1
-    names = log_featurenames+hive_featurenames[1:-1]+['UnoRoomPlayReviewOneLog_postusetime_four_ratio','UnoRoomPlayReviewOneLog_unomay_four_ratio']
+    df['UnoRoomPlayReviewOneLog_unomay_four_ratio']= df['UnoRoomPlayReviewOneLog_unomay']/df['unocount']
+    df['UnoRoomPlayReviewOneLog_unomay_four_ratio'][df['unocount']<1]=-1
+    df['UnoRoomPlayReviewOneLog_argue_four_allhappenratio']= df['arguehappencount']/df['arguecount']
+    df['UnoRoomPlayReviewOneLog_argue_four_allhappenratio'][df['arguecount']<0]=-1
+    df['UnoRoomPlayReviewOneLog_argue_four_allhappenratio'][df['arguecount']==0]=0
+    df['UnoRoomPlayReviewOneLog_argue_four_allsuccessratio']= df['arguesuccesscount']/df['arguehappencount']
+    df['UnoRoomPlayReviewOneLog_argue_four_allsuccessratio'][df['arguehappencount']<1]=-1
+    df['UnoRoomPlayReviewOneLog_argue_four_allsuccessratio'][df['arguehappencount']==0]=0
+
+    names = log_featurenames+hive_featurenames[1:-1]+['UnoRoomPlayReviewOneLog_postusetime_four_ratio','UnoRoomPlayReviewOneLog_unomay_four_ratio'
+                                                      ,'UnoRoomPlayReviewOneLog_argue_four_allhappenratio','UnoRoomPlayReviewOneLog_argue_four_allsuccessratio']
     df = df[names]
     df.fillna(value=0, inplace=True)
     df.to_csv(output_filename, sep='|',index=False,header=False)
+
+
+    feature_dict={}
+    f = open('feature_dict.txt','r')
+    feature_dict_datas=f.read().split('\t')
+    for feature_dict_data in feature_dict_datas:
+        feature_dict_L = feature_dict_data.split('\t')
+        feature_dict[feature_dict_L[0]]='\t'.join([feature_dict_L[1],feature_dict_L[2],feature_dict_L[3],feature_dict_L[4]])
+
 
 
     with open(log_featurename_filename,'w') as f2:
@@ -110,7 +132,7 @@ if __name__=='__main__':
         print(names)
         tmp = zip(names,range(0,len(names)))
         f2.write('序号\t名字\t描述\t重要级\tNone值\tDefault建议值\n')
-        f2.write('\n'.join(['\t'.join([str(x[1]),x[0],'详见xx','1','-1','0']) for x in tmp]))
+        f2.write('\n'.join(['\t'.join([str(x[1]),x[0],'详见xx' if str(x[1]) not in feature_dict_L else feature_dict_L[str(x[1])],'1','-1','0']) for x in tmp]))
 
 
 
