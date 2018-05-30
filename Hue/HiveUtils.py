db5cf7686f0e7f5573925ff24d7bbc72fea63677
kuanke <asdqsczser@gmail.com>
2018-05-30 14:04:55 +0800

    debug

--- Hue/HiveUtils.py
+++ Hue/HiveUtils.py
@@ -53,54 +53,32 @@ if __name__ == '__main__':
 
     for t in range(0,1):
         date= '2018-05-'+str(15+t)
-        sql = '''select * from la_shitu_analysis.shitu_pve_pair '''
+        sql = ''' select 
+                    a.server,a.role_id,a.d_role_id,a.seq
+                    ,b.churndate,c.chatinfo,d.chatinfo,e.tradeinfo,f.tradeinfo,g.pveinfo
+                  from la_shitu_analysis.shitu_seq a
+                  left outer join
+                    la_shitu_analysis.shitu_churn b 
+                  on a.role_id=b.role_id
+                  left outer join
+                    la_shitu_analysis.shitu_chat_pair c
+                  on a.role_id=c.role_id and a.d_role_id=c.d_role_id
+                  left outer join
+                    la_shitu_analysis.shitu_chat_pair_reversed d
+                  on a.role_id=d.role_id and a.d_role_id=d.d_role_id
+                  left outer join
+                    la_shitu_analysis.shitu_trade_pair e
+                  on a.role_id=e.role_id and a.d_role_id=e.d_role_id
+                  left outer join
+                    la_shitu_analysis.shitu_trade_pair_reversed f
+                  on a.role_id=f.role_id and a.d_role_id=f.d_role_id
+                  left outer join
+                    la_shitu_analysis.shitu_pve_pair g
+                  on a.role_id=g.role_id and a.d_role_id=g.d_role_id 
+              '''
         result = hive_client.query(sql)
-        f=open('shitu_pve_pair.txt','a+')
+        f=open('shitu_all.txt','a+')
         f.write('\n'.join(['\t'.join([str(y) for y in x]) for x in result]))
         f.write('\n')
         f.close()
-
-        sql = '''select * from la_shitu_analysis.shitu_trade_pair_reversed '''
-        result = hive_client.query(sql)
-        f=open('shitu_trade_pair_reversed.txt','a+')
-        f.write('\n'.join(['\t'.join([str(y) for y in x]) for x in result]))
-        f.write('\n')
-        f.close()
-
-        sql = '''select * from la_shitu_analysis.shitu_trade_pair '''
-        result = hive_client.query(sql)
-        f=open('shitu_trade_pair.txt','a+')
-        f.write('\n'.join(['\t'.join([str(y) for y in x]) for x in result]))
-        f.write('\n')
-        f.close()
-
-        sql = '''select * from la_shitu_analysis.shitu_chat_pair '''
-        result = hive_client.query(sql)
-        f=open('shitu_chat_pair.txt','a+')
-        f.write('\n'.join(['\t'.join([str(y) for y in x]) for x in result]))
-        f.write('\n')
-        f.close()
-
-        sql = '''select * from la_shitu_analysis.shitu_chat_pair_reversed '''
-        result = hive_client.query(sql)
-        f=open('shitu_chat_pair_reversed.txt','a+')
-        f.write('\n'.join(['\t'.join([str(y) for y in x]) for x in result]))
-        f.write('\n')
-        f.close()
-
-        sql = '''select * from la_shitu_analysis.shitu_churn '''
-        result = hive_client.query(sql)
-        f=open('shitu_churn.txt','a+')
-        f.write('\n'.join(['\t'.join([str(y) for y in x]) for x in result]))
-        f.write('\n')
-        f.close()
-
-        sql = '''select * from la_shitu_analysis.shitu_seq '''
-        result = hive_client.query(sql)
-        f=open('shitu_seq.txt','a+')
-        f.write('\n'.join(['\t'.join([str(y) for y in x]) for x in result]))
-        f.write('\n')
-        f.close()
-
-
     hive_client.close()
