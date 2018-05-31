29fb5a434c65cda17f10e9cd660a2c07d93db3a3
kuanke <asdqsczser@gmail.com>
2018-05-31 14:12:18 +0800

    debug

--- Hue/HiveUtils.py
+++ Hue/HiveUtils.py
@@ -52,23 +52,16 @@ if __name__ == '__main__':
 
     d = datetime.datetime.strptime('2018-04-01', '%Y-%m-%d')
     for t in range(0,60):
-        date = (d+datetime.timedelta(t)).strftime('%Y-%m-%d')
-        sql = ''' 
-                Insert overwrite table qn_guanning.guanningpersonal
+        date = d+datetime.timedelta(t).strftime('%Y-%m-%d')
+        sql = '''
+                    Insert overwrite table qn_guanning.guanninggame
                     partition (ds='%s')
-                    select server,id,
-                    concat_ws(',',cast(avg(iswin) as string),cast(avg(xiulian) as string)
-                    ,cast(avg(total_score) as string),cast(avg(help_num) as string)
-                    ,cast(avg(kill_score) as string),cast(avg(flag_score) as string)
-                    ,cast(avg(xiuwei) as string),cast(avg(equip_score) as string)
-                    ,cast(avg(team_score) as string),cast(avg(killed_score) as string)
-                    ,cast(avg(score_count) as string),cast(avg(grade) as string)
-                    ,cast(avg(class) as string),cast(count(*) as string))
-                    from  qndb.h_guanning_result where ds>='%s' and ds<='%s'
-                    group by server,id
-              '''%(date,(d+datetime.timedelta(6+t)).strftime('%Y-%m-%d'),(d+datetime.timedelta(7+t)).strftime('%Y-%m-%d'))
+                    SELECT min(time),server,iid,concat_ws(',',collect_set(concat_ws(':',cast(id as STRING),cast(iswin as String))))
+                    from qndb.h_guanning_result
+                    where ds='%s'
+                    group by server,iid
+                  '''%(date,date)
         result = hive_client.action(sql)
-        print t
         # f=open('shitu_all.txt','a+')
         # f.write('\n'.join(['\t'.join([str(y) for y in x]) for x in result]))
         # f.write('\n')
@@ -79,5 +72,34 @@ if __name__ == '__main__':
 
 
 
+# d = datetime.datetime.strptime('2018-04-01', '%Y-%m-%d')
+# for t in range(0,60):
+#     date = d+datetime.timedelta(t).strftime('%Y-%m-%d')
+#     sql = '''
+#                 Insert overwrite table qn_guanning.guanninggame
+#                 partition (ds='2018-05-30')
+#                 SELECT min(time),server,iid,concat_ws(',',collect_set(concat_ws(':',cast(id as STRING),cast(iswin as String))))
+#                 from qndb.h_guanning_result
+#                 group by server,iid
+#               '''%(date,date)
+#     result = hive_client.action(sql)
 
-
+# d = datetime.datetime.strptime('2018-04-01', '%Y-%m-%d')
+# for t in range(0,60):
+#     date = (d+datetime.timedelta(t)).strftime('%Y-%m-%d')
+#     sql = '''
+#                 Insert overwrite table qn_guanning.guanningpersonal
+#                     partition (ds='%s')
+#                     select server,id,
+#                     concat_ws(',',cast(avg(iswin) as string),cast(avg(xiulian) as string)
+#                     ,cast(avg(total_score) as string),cast(avg(help_num) as string)
+#                     ,cast(avg(kill_score) as string),cast(avg(flag_score) as string)
+#                     ,cast(avg(xiuwei) as string),cast(avg(equip_score) as string)
+#                     ,cast(avg(team_score) as string),cast(avg(killed_score) as string)
+#                     ,cast(avg(score_count) as string),cast(avg(grade) as string)
+#                     ,cast(avg(class) as string),cast(count(*) as string))
+#                     from  qndb.h_guanning_result where ds>='%s' and ds<='%s'
+#                     group by server,id
+#               '''%(date,(d+datetime.timedelta(6+t)).strftime('%Y-%m-%d'),(d+datetime.timedelta(7+t)).strftime('%Y-%m-%d'))
+#     result = hive_client.action(sql)
+#     print t
